fcc2561255a1a76d3aee55d5aa6969141fe620c6
Matt Hogan <54578787+matthogan40@users.noreply.github.com>
2023-11-03 16:20:44 +0000

    Added code tracing example

---
+++ en/code/code_tracing/main.py
@@ -0,0 +1,8 @@
+# Emojis to copy and paste into your code 
+# 📅🕒🎨🎮🔬🎉🕶️🎲🦄🚀💯⭐💛
+# 😃😜❤️📚⚽🎾👟♻️🌳✨🥺🌈🔥🐍🌏  
+  
+# Put code to run under here
+
+print ("Hello 🌏")
+Print ("This is my first program usin Python 🐍")
