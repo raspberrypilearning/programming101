18a9dc57008b8e99541aa2fecd0878779d6f1335
Matt Hogan <54578787+matthogan40@users.noreply.github.com>
2023-11-03 16:45:01 +0000

    Added primm proof

---
+++ en/code/primm/main.py
@@ -0,0 +1,4 @@
+# Get user input for square side length
+side_length = float(input("Enter the side length of the square: "))
+area_square = side_length ** 2
+print("Area of the square:", area_square)
