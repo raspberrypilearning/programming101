8b41323ef3bee2e63af236d8f38bfb8aa7d7dd0c
m-bowley <mac@raspberrypi.org>
2023-12-14 15:57:54 +0000

    adding second example

---
+++ en/code/while_loop_guessing_game/main.py
@@ -0,0 +1,11 @@
+playing = True
+number = 89
+
+while playing:
+    guess = input("Guess a number between 1 and 100 > ")
+    guess_int = int(guess)
+    if guess == number:
+        print("You got it right!")
+        playing = False
+    else: 
+        print("Try again!")
