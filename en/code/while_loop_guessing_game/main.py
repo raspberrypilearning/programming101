273ff8770f8de00989948d45b973e5296ba259d1
m-bowley <mac@raspberrypi.org>
2024-01-04 13:31:55 +0000

    Fixing error

--- en/code/while_loop_guessing_game/main.py
+++ en/code/while_loop_guessing_game/main.py
@@ -5,7 +5,7 @@ while playing == True:
     guess = input("Guess a number between 1 and 100 > ")
     guess_int = int(guess)
     
-    if guess == number:
+    if guess_int == number:
         print("You got it right!")
         playing = False
     else: 
