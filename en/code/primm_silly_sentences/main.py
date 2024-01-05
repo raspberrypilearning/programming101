a335755554e859ff288ff9832e4646e38c0c2f39
Matt Hogan <54578787+matthogan40@users.noreply.github.com>
2024-01-05 15:25:40 +0000

    added parsons silly sentences

---
+++ en/code/primm_silly_sentences/main.py
@@ -0,0 +1,8 @@
+noun = "Car"
+adverb = "gently"
+adjective = "loud"
+print(f"The {noun} was {adjective} when it {adverb} went to school")
+noun = "Zebra"
+adverb = "aggressively"
+adjective = "giant"
+print(f"The {noun} was {adjective} when it {adverb} went to school")
