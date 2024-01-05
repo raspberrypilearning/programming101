1a1b9d4fd54d797112f6ebf90fe5070022e8311b
Matt Hogan <54578787+matthogan40@users.noreply.github.com>
2024-01-05 16:05:52 +0000

    mixed the lines up on week1 code for a parsons problem

--- en/code/parsons_sequence/main.py
+++ en/code/parsons_sequence/main.py
@@ -1,10 +1,10 @@
-print("Welcome to The Adventure")
+print(f'The gold you find will be worth {gold_value} points.')
 character = input("What is your character's name ")
 print(f'{character} is on a quest to find gold and secrets.')
 difficulty = input(f"What difficulty level should {character} play? (1-10) ")
 gold_value = 100
+print("Welcome to The Adventure")
+print(f'Your health will drop by {health_drop} points each turn.')
 health_drop = 1
 health_drop = health_drop * difficulty
-print(f'Your health will drop by {health_drop} points each turn.')
 gold_value = gold_value / difficulty
-print(f'The gold you find will be worth {gold_value} points.')
