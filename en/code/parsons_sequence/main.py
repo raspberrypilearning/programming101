78c6264efc342dd9e3a5c68ba4e55c98539b8377
Matt Hogan <54578787+matthogan40@users.noreply.github.com>
2024-01-05 15:11:59 +0000

    Added parsons sequence

---
+++ en/code/parsons_sequence/main.py
@@ -0,0 +1,4 @@
+print("Along the way, they encountered challenges 🥺 and made friends from different backgrounds.")
+print("Once upon a time, there lived an adventurer.")
+print("After many adventures, the adventurer found an remote island to live the rest of his life .")
+print("They started on a journey to explore the world 🌏 and its diverse cultures.")
