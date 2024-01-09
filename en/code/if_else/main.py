ad15fe464fc7e0cdd3a193b3d6469f8a4f8694f4
Matt Hogan <54578787+matthogan40@users.noreply.github.com>
2024-01-09 10:26:18 +0000

    Added if else

---
+++ en/code/if_else/main.py
@@ -0,0 +1,13 @@
+# Get input from the user
+response = input("Are you over the age of 13?")
+
+# Check different responses
+if response == "yes":
+    # If true, welcome the user
+    print("That is great! 👋 Welcome to your new video streaming service")
+else:
+    # If false, inform the user about the age restriction
+    print("Sorry, you have to be over the age of 13 to use this video streaming service")
+
+# Display a message
+print("Please wait")
