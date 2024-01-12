57c25d91430e04f74230bb5e5873639893decb07
Matt Hogan <54578787+matthogan40@users.noreply.github.com>
2024-01-12 15:26:37 +0000

    added set looping number

---
+++ en/code/set_looping_number/main.py
@@ -0,0 +1,7 @@
+shopping = []
+
+for item_number in range(5):
+    item = input("What is the item number " + str(item_number) +"?  ")
+    shopping.append(item)
+
+print(f"Your shopping list: {shopping}") 
