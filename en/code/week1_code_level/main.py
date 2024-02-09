0a6104f96b43580e1233ce302e8cee5a25f4f73c
Matt Hogan <54578787+matthogan40@users.noreply.github.com>
2024-02-09 15:50:58 +0000

    updates week one code level

--- en/code/week1_code_level/main.py
+++ en/code/week1_code_level/main.py
@@ -1,21 +1,9 @@
-from datetime import date
-
-# Prompt the user to enter their date of birth in English format (dd/mm/yyyy)
-date_of_birth = input("Enter your date of birth (dd/mm/yyyy): ")
-
-# Extract the year, month, and day from the input
-day, month, year = map(int, date_of_birth.split('/'))
-
-# Check if the user is over 18
-if age >= 18:
-    print("You are not over 18 years old.")
-else:
-    print("You are over 18 years old.")
-    
-# Get the current date
-
-current_date = date.today()
-
-# Calculate the user's age
-age = current_date.year - year - ((current_date.month, current_date.day) < (month, day))
-
+gold_value = 100
+health_drop = 1 * difficulty
+print(f'{character} is on a quest to find gold and secrets.')
+gold_value = gold_value / difficulty
+print("Welcome to The Adventure")
+difficulty = int(input(f"What difficulty level should {character} play? (1-10) "))
+print(f'The gold you find will be worth {gold_value} points.')
+character = input("What is your character's name? ")
+print(f'Your health will drop by {health_drop} points each turn.')
